964b20149b1cab385054fa3878192af633e3114a
Zhouyang Fang <fangzhy1998@gmail.com>
2022-04-21 16:24:25 -0400

    fixed acceleration

--- assignment_package/src/scene/player.h
+++ assignment_package/src/scene/player.h
@@ -21,9 +21,9 @@ public:
 
 
 	bool isFlight;
-    float acceleration = 0.1f;
+    float acceleration = 1.f;
     float jumpSpeed = 50.f;
-    float friction = 0.7f;
+    float friction = 0.01f;
     float g = 0.5f;
 
     glm::vec3 mcr_posPrev;
